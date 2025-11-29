6bf2fa028ebb8d7ab42e5fb1d9b5f8ce9d5ac0f3
shawonmondol-shibu <shawonmondolshibu@gmail.com>
2025-11-29 20:05:47 +0600

    proxy updated

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
  
-const allowedOrigins = ['https://api.winaclaim.com/']
+const allowedOrigins = ['*']
  
 const corsOptions = {
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
