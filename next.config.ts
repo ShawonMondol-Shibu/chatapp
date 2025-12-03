da7b84b3912b6601f1f8599d00dd25327424b5d7
shawonmondol-shibu <shawonmondolshibu@gmail.com>
2025-12-03 19:54:51 +0600

    crossOrigin updated

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://api.winaclaim.com" },
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-        ],
-      },
-    ];
-  },
+  crossOrigin: "anonymous"
 };
 
 export default nextConfig;
